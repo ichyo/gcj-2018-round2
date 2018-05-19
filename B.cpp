2172e6b47d2182b7d9bdb4291133a13eea2116e1
ichyo <ichyo00@gmail.com>
2018-05-20 00:32:18 +0900

    Submit B

--- B.cpp
+++ B.cpp
@@ -14,14 +14,48 @@ void iostream_init() {
     cout.precision(12);
 }
 
+const int MAX_N = 505;
+const int MAX_S = 50;
+int dp[MAX_N][MAX_N];
+void init() {
+    set<pair<int, int>> cand;
+    REP(i, MAX_N) if(i != 0) cand.insert(make_pair(0, i));
+    REP(i, MAX_N) if(i != 0) cand.insert(make_pair(i, 0));
+    REP(i, MAX_S) REP(j, MAX_S) if(i + j != 0) cand.insert(make_pair(i, j));
+    memset(dp, -1, sizeof(dp));
+    dp[0][0] = 0;
+    int iter = 0;
+    for(auto p : cand) {
+        cerr << iter++ << "/" << cand.size() << endl;
+        const int A = p.first;
+        const int B = p.second;
+        for(int i = MAX_N - A - 1; i >= 0; i--) {
+            for(int j = MAX_N - B - 1; j >= 0; j--) {
+                if (dp[i][j] != -1) {
+                    dp[i + A][j + B] = max(dp[i + A][j + B], dp[i][j] + 1);
+                }
+            }
+        }
+    }
+    //REP(i, MAX_N) REP(j, MAX_N) assert(dp[i][j] != -1);
+    long long sum = 0;
+    REP(i, MAX_N) REP(j, MAX_N) sum += (long long)dp[i][j] * i * j;
+    cerr << sum << endl;
+}
+
 void solve() {
+    int R, B;
+    cin >> R >> B;
+    cout << dp[R][B] << endl;
 }
 
 int main(){
     iostream_init();
+    init();
     int TC;
     cin >> TC;
     for(int testcase = 1; testcase <= TC; testcase++) {
+        cout << "Case #" << testcase << ": ";
         solve();
     }
     return 0;
