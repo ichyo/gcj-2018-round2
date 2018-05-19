352a6ef387db4bf1f008607243cae81ad4b72081
ichyo <ichyo00@gmail.com>
2018-05-19 22:59:30 +0900

    Add B C D

---
+++ C.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define REP(i,n) for(int i=0; i<(int)(n); ++i)
+using namespace std;
+typedef long long LL;
+
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+inline bool valid(int x, int w) { return 0 <= x && x < w; }
+
+void iostream_init() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.setf(ios::fixed);
+    cout.precision(12);
+}
+
+void solve() {
+}
+
+int main(){
+    iostream_init();
+    int TC;
+    cin >> TC;
+    for(int testcase = 1; testcase <= TC; testcase++) {
+        solve();
+    }
+    return 0;
+}
